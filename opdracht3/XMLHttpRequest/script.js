14d01166d5b520ca0c269c8cdaf45e4019cd4693
koopReynders <mail@kopo.nl>
2019-04-16 14:41:13 +0200

    xmlhttprequest example

--- opdracht3/XMLHttpRequest/script.js
+++ opdracht3/XMLHttpRequest/script.js
@@ -1,88 +1,64 @@
-
-//script.js
-//console.log(this);
+console.log("huh?",this);
 //var uri = "https://api.data.amsterdam.nl/panorama/recente_opnames/2018/?format=json";
 //var uri = "https://open.data.amsterdam.nl/Attracties.json";
 //var uri = "https://open.data.amsterdam.nl/Activiteiten.json";
-//var uri = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
-//var uri = "http://dennistel.nl/movies"; //online, geen https
-var uri = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json'; //json file op github
-var button = document.querySelector("button");
-var loaderElement = document.querySelector("span");
+//var uri = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
+
+var uri = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+
+var title = document.querySelector('h1');
 var section = document.querySelector('section');
-//console.log("loaderElement",loaderElement);
+var button = document.querySelector("button");
+var loaderElement = document.querySelector(".loader");
+console.log("loader",loaderElement);
 
 ////https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
 function showData(jsonObj) {
-  var films = jsonObj;
-  console.log("showData films",films);
+  var data = jsonObj;
+  console.log("JsonObj data",data);
+  console.log("squadName",data['squadName'])
 
-  for (var i = 0; i < films.length; i++) {
-    console.log("film " + i);
-    var filmpiekijken = document.createElement('article');
+  //data uit de json halen
+  //var squadName = document.createElement('h2');
+  title.textContent = data['squadName'];
+  //var homeTown = document.createElement('p');
+  title.textContent += " - " + data['homeTown'];
+  title.textContent += " - " + data['secretBase'];
+  //header.appendChild(squadName);
+  //header.appendChild(homeTown);
 
-    //TITEL, COVER EN BESCHRIJVING
-    var filmtitel = document.createElement('h2');
-      filmtitel.textContent = films[i].title;
-    var filmplot = document.createElement('p');
-      filmplot.textContent = films[i].simple_plot;
-    var filmcover = document.createElement('img');
-      filmcover.src = films[i].cover;
-    //myImg.textContent = films[i].cover;
-    //console.log(filmcover.src);
+  var members = jsonObj['members'];
+  console.log("members",members);
 
-    //GENRES
-    var genres = films[i].genres;
-    for (var n = 0; n < genres.length; n++) {
-      console.log("genre: ",genres[n]);
-    } //end for genres
+  for (var i = 0; i < members.length; i++) {
+    var superhero = document.createElement('article');
 
-    //REVIEWS
-    var reviewslezen = document.createElement('ul');
-    var reviewsbutton = document.createElement('button');
-      reviewsbutton.textContent = "Lees reviews";
-    //reviewslezen.appendChild(reviewsheader);
-    var reviews = films[i].reviews;
-    for (var j = 0; j < reviews.length; j++) {
-      var listItem = document.createElement('li');
-      listItem.textContent = reviews[j].score + ' - ' + reviews[j].created_at;
-      reviewslezen.appendChild(listItem);
-    } //end: for reviews
+    var name = document.createElement('h2');
+    name.textContent = members[i].name;
 
+    var secretIdentity = document.createElement('p');
+    secretIdentity.textContent = "secret name: " + members[i].secretIdentity;
 
-    //functies om elementen toe te voegen
-    reviewsbutton.info = reviewslezen; //zo kun je een variabele aanmaken in ene object om iets te 'bewaren', bijvoorbeel een verwijzing naar een ander element
-    reviewsbutton.onclick = function(){
-      console.log("click this =" + this.info, this); //met this, verwijs je naar het object zelf waar de functie wordt uitgevoerd, hier gebruik ik de variabale die in in het object heb aangemaakt
-      //console.log("parentNode",this.parentNode);
-      this.info.classList.toggle('show')
-    } //end: reviewsheader onClick
-    reviewslezen.addEventListener("click", function(){
-      console.log("click this =", this);
-      console.log("Wat is de parentNode?",this.parentNode); //met 'parentNode' kun je een element met een relatief pad bereiken
-      console.log("Wat zijn de childNodes?",this.childNodes); //met 'childNodes' kun je de kinderen van een element met een relatief pad bereiken
-      var button = this.parentNode.querySelector('button'); //relatief pad gebruiken voor een actie die moet plaatsvinden
-      console.log("Is dit de button?",button);
-    }); //end: reviewslezen click
+    var age = document.createElement('span');
+    age.textContent = "leeftijd: " + members[i].age;
 
+    var powers = document.createElement('ul');
+    var superpowers = members[i].powers;
+    for(var j =0; j<superpowers.length; j++){
+      console.log("superpowers",superpowers[j])
+      var kracht = document.createElement('li');
+      kracht.textContent = superpowers[j];
+      powers.appendChild(kracht);
+    }
 
+    superhero.appendChild(name);
+    superhero.appendChild(secretIdentity);
+    superhero.appendChild(age);
+    superhero.appendChild(powers);
 
-
-    //ALLE DATA KOPPELEN
-    filmpiekijken.appendChild(filmtitel);
-    filmpiekijken.appendChild(filmplot);
-    filmpiekijken.appendChild(filmcover);
-    filmpiekijken.appendChild(reviewsbutton);
-    filmpiekijken.appendChild(reviewslezen);
-
-    //HTML INJECTION IN BESTAANDE SECTION
-    section.appendChild(filmpiekijken);
-
-
-
-
-  } //end: for films
-} //end: function showData
+    section.appendChild(superhero);
+  }
+} //end: function showdata
 
 //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
 //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
@@ -94,48 +70,46 @@ function loadimagesmetXHR(){
   request.send();
 
   request.addEventListener("load", function(){
-    //console.log("request load: ",request.response);
-    loaderElement.classList.remove('show');
-    console.log("XHR data",request.response);
-    console.table(request.response);
+    console.log("request is geladen: ",request.response);
+    //als de data geladen is, mag de loader weg:
+    loaderElement.classList.add('hide');
+    //functie aanroepen om van de data html te maken
     showData(request.response);
   });
 //  request.onload = function() {
 //      console.log("request.onload: ",request.response);
 //    }
-/*request.timeout = 10000; // time in milliseconds
+  request.timeout = 10000; // time in milliseconds
   request.ontimeout = function(e) {
     // XMLHttpRequest timed out. Do something here.
     console.log("ontimeout: " +request.timeout+", het laden duurt te lang !",e);
   };
-  */
   request.onerror = function() {
       console.log('Fetch Error', request.status);
   };
-}
-//loadimagesmetXHR();
+} //end: function loadimagesmetXHR();
 
+
+//loader feedback eerst onzichtbaar maken
+loaderElement.classList.add('hide');
 //actie
 button.onclick = function(){
-  loaderElement.classList.add('show');
-  //this.classList.add('hide');
-  //hier iets doen met de button die laadt, dan weer gewoon een button wordt ...
-
+  loaderElement.classList.remove('hide'); //loader wordt zichtbaar
+  this.classList.add('hide'); //button mag weg
   section.innerHTML = ""; //main leeghalen. just in case
   loadimagesmetXHR();
-};
+}; //end: button.onclick
 
 
 
 
 
-
+/*
 function loadRestApiFetch(){ //Rest Api call met Fetchs
   console.log("function loadRestApiFetch");
 
   loaderElement.classList.add('show');
-  fetch(uri)
-    .then(function(response) {
+  fetch(uri).then(function(response) {
       console.log(response.headers.get('Content-Type'));
       console.log(response.headers.get('Date'));
 
@@ -145,17 +119,17 @@ function loadRestApiFetch(){ //Rest Api call met Fetchs
       console.log(response.url);
 
       return response.json();
-    })
-    .then(function(myJson) {
+    }).then(function(myJson) {
       console.log('Request successful', myJson);
       //eerst de loader weg halen !
       loaderElement.classList.remove('show');
       //dan de html renderen
       //document.querySelector("p").innerHTML="joehoe";
       //console.log(myJson);
-    })
-    .catch(function(error) {
+    }).catch(function(error) {
       console.log('Request failed', error)
     });
 }
 //loadRestApiFetch();
+
+*/
