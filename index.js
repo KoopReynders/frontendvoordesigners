eb0243890a8b6b2d6f6f79270a464a6b0e4bc779
WesselSmit <45405413+WesselSmit@users.noreply.github.com>
2019-04-19 14:49:53 +0200

    les 1
    Code na de eerste les Front-end

---
+++ index.js
@@ -0,0 +1,5 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+//IDEE: make a working version with the inline HTML => make it with JS
