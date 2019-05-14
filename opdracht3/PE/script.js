f82a12641cd17b1e90568bf2e3614a2932c072ca
koopReynders <mail@kopo.nl>
2019-05-14 15:42:54 +0200

    load html data

--- opdracht3/PE/script.js
+++ opdracht3/PE/script.js
@@ -2,18 +2,22 @@
 
 var section = document.querySelector('section');
 var button = document.querySelector('button');
-var requestURL = 'data.html';
+//var requestURL = 'data.html';
 var requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/PE/data.html';
 
-
 button.addEventListener('click',function(){
+  section.textContent = "load data";
   // console.log('click');
   var request = new XMLHttpRequest();
   request.open('GET', requestURL);
-  request.responseType = 'document';
+  request.responseType = 'text';
   request.send();
   request.onload = function() {
-    console.log("request.response",request.response);
+    var data = request.response;
+    console.log('request.responseType',request.responseType)
+    console.log("request.response", data);
+
+    section.innerHTML = data;
   } //end request.onload
 
 }) //end button.addEventListener
