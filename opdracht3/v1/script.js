29e71e957b2df234495b52a57e4f2c94e267fa41
Koop Reynders <mail@kopo.nl>
2020-03-23 15:54:40 +0100

    video test op JSON

--- opdracht3/v1/script.js
+++ opdracht3/v1/script.js
@@ -12,9 +12,44 @@ request.responseType = 'json';
 request.send();
 
 request.addEventListener("load", function(){
-  var data = request.response;
+  // var data = request.response;
+    //er is data
   console.log("request is geladen: ",request.response);
-  //er is data
+
   //nu kun je iets doen
-  section.textContent = JSON.stringify(data);
+  renderHTML(request.response);
+
 });
+
+function renderHTML(data){
+  console.log("renderHTML")
+  //section.textContent = JSON.stringify(data);
+
+  for (var i = 0; i < data.length; i++) {
+    var article = document.createElement('article');
+
+    var title = document.createElement('h2');
+    title.textContent = data[i].title;
+
+    var plot = document.createElement('p');
+    plot.textContent = data[i].simple_plot;
+
+    var cover = document.createElement('img');
+    cover.src = data[i].cover;
+
+    var video = document.createElement('video');
+    video.src = data[i].trailer;
+    video.controls = "true";
+    video.width = "360";
+    video.poster = data[i].cover;
+
+    article.appendChild(title);
+    article.appendChild(plot);
+    //article.appendChild(cover);
+    article.appendChild(video);
+
+    section.appendChild(article);
+  };
+
+
+}
