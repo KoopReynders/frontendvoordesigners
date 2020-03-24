934068b129d6e0f137503da4b6e24e0156bfdc8d
Koop Reynders <mail@kopo.nl>
2020-03-24 20:57:48 +0100

    comments

--- opdracht3/PEmenu/script.js
+++ opdracht3/PEmenu/script.js
@@ -1,22 +1,19 @@
 //cloon menu voor fancy menu
 
-//menu link
-var menulink = document.querySelector("header > a");
-//in de header wil ik het menu plaatsen
-// var doc = document.querySelector("body");
 //menu pakken en clonen
-var menu = document.querySelector("#menu");
-var menucloon = menu.cloneNode(true);
-//het gecloonde menu in de header plaatsen.
+// var menu = document.querySelector("#menu");
+var menucloon = document.querySelector("#menu").cloneNode(true);
+//het gecloonde menu in de body plaatsen.
 document.querySelector("body").appendChild(menucloon);
 //id zetten (anders heb je twee dezelfde id's in de dom)
 menucloon.id ="menutop";
+//titel mag anders omdat dit menu boevnin de pagina wordt getoond
 menucloon.querySelector("h2").textContent = "Fancy menu:";
 //class toevoegen voor de css magic
 menucloon.classList.add("hamburgesia");
 
-//a link menu actie afpakken
-menulink.onclick = function() {
+//a link menu afpakken en nieuwe actie aanmaken
+document.querySelector("header > a").onclick = function() {
   event.preventDefault();
   menucloon.classList.toggle('showmenu')
 };
